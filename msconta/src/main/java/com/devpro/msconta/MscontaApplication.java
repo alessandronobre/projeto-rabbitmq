6e52c1438a2588c06e48bcc1f7bd16fd040a7a02
Alessandro <alessandronobre.ti@gmail.com>
2023-12-22 21:06:57 -0300

    Subindo microservico de conta bancaria

---
+++ msconta/src/main/java/com/devpro/msconta/MscontaApplication.java
@@ -0,0 +1,13 @@
+package com.devpro.msconta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscontaApplication.class, args);
+	}
+
+}
---
+++ msconta/src/test/java/com/devpro/msconta/MscontaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpro.msconta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscontaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
