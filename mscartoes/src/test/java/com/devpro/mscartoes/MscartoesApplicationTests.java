eb6530fc7b469c991204b213e798334a81d74759
Alessandro <alessandronobre.ti@gmail.com>
2023-12-22 21:09:17 -0300

    Subindo microservico de cartoes

---
+++ mscartoes/src/test/java/com/devpro/mscartoes/MscartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpro.mscartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
